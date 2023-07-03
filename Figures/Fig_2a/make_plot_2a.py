ebf398db78e0b1a91720f085878b861e04abb085
root <root@LAPTOP-H96VTBEJ.localdomain>
2023-07-03 01:35:49 -0700

    edited Figure2a to generate average plot

--- Figures/Fig_2a/make_plot_2a.py
+++ Figures/Fig_2a/make_plot_2a.py
@@ -16,6 +16,17 @@ def get_mark_cycler():
                  cycler(marker=["4", "2", "3", "1", "+", "x", "."]))
     return marker_cycler
 
+
+def get_hist_avg(avg_num, hist):
+    hist_avg = []
+    for idx in list(range(len(hist) - avg_num + 1)):
+        avg = 0
+        for i in range(idx, idx + avg_num):
+            avg += hist[i]
+        avg = avg / avg_num
+        hist_avg.append(avg)
+    return hist_avg
+
 def make_avg_plot():
     """
     lets say the HR distance we measure are
@@ -38,25 +49,21 @@ def make_avg_plot():
             param_idx_avg += param_idx_l[i]
         param_idx_avg =  param_idx_avg / avg_num
         param_idx_l_avg.append(param_idx_avg)
-    HR_dist_hist_avg = []
-    for idx in list(range(len(HR_dist_hist)- avg_num + 1)):
-        HR_dist_avg = 0
-        for i in range(idx, idx+avg_num):
-            HR_dist_avg += HR_dist_hist[i]
-        HR_dist_avg = HR_dist_avg/avg_num
-        HR_dist_hist_avg.append(HR_dist_avg)
+    HR_dist_hist_avg = get_hist_avg(avg_num, HR_dist_hist)
+    E_hist_avg = get_hist_avg(avg_num, E_hist)
+    fid_hist_avg = get_hist_avg(avg_num, fid_hist)
 
 
     plt.rcParams['font.family'] = 'serif'
     plt.rcParams['font.serif'] = ['Times New Roman'] + plt.rcParams['font.serif']
     fig, ax = plt.subplots()
-    ax.scatter(param_idx_l, E_hist, alpha = 1, c = "#0072B2", s = 20, marker = ".", label = "Energy")
+    ax.scatter(param_idx_l_avg, E_hist_avg, alpha = 1, c = "#0072B2", s = 20, marker = ".", label = "Energy")
     ax.set_xlabel('VQE Iterations', fontsize = 12)
     ax.set_ylabel("Energy", fontsize = 12)
     ax.legend(bbox_to_anchor=(1.28, 1.30), fontsize = 12)
     ax2 = ax.twinx()
     ax2.scatter(param_idx_l_avg, HR_dist_hist_avg, alpha = 1, c = "#D55E00", s = 20, marker=".", label = "HR distance")
-    ax2.scatter(param_idx_l, fid_hist, c = '#009E73', alpha = 0.8, marker=".", label = "Fidelity")
+    ax2.scatter(param_idx_l_avg, fid_hist_avg, c = '#009E73', alpha = 0.8, marker=".", label = "Fidelity")
     ax2.set_ylabel("HR distance | Fidelity", fontsize = 12)
     ax2.legend(bbox_to_anchor=(1.28, 1.22), fontsize = 12)
     plt.savefig(f"plot_2a_avg_{avg_num}.svg", dpi = 300, bbox_inches='tight')
