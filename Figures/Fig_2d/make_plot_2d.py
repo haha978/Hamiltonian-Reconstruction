49d7fc486fff993e439e21e5d56560b8f40eadec
root <root@LAPTOP-H96VTBEJ.localdomain>
2023-07-03 02:01:14 -0700

    Figure 2 done

--- Figures/Fig_2d/make_plot_2d.py
+++ Figures/Fig_2d/make_plot_2d.py
@@ -16,6 +16,47 @@ def get_mark_cycler():
                  cycler(marker=["4", "2", "3", "1", "+", "x", "."]))
     return marker_cycler
 
+def get_hist_avg(avg_num, hist):
+    hist_avg = []
+    for idx in list(range(len(hist) - avg_num + 1)):
+        avg = 0
+        for i in range(idx, idx + avg_num):
+            avg += hist[i]
+        avg = avg / avg_num
+        hist_avg.append(avg)
+    return hist_avg
+
+def make_avg_plot():
+    """
+    function that creates a plot that averages HR distances to show better correlation with energy
+    truncates the plot at an appropriate energy if needed
+    """
+    #SET avg_num HERE:
+    avg_num = 2
+    HR_dist_hist = load_l("HR_10000shots_aer_simulator_p1_0.0065_p2_0.0398.pkl")
+    E_hist = load_l("noisy_E_10000_shots_aer_simulator__p1_0.0065_p2_0.0398.pkl")
+    fid_hist = load_l("fid_p1_0.0065_p2_0.0398.pkl")
+    param_idx_l = np.array(list(range(len(E_hist))))
+
+    HR_dist_hist_avg = get_hist_avg(avg_num, HR_dist_hist)
+    E_hist_avg = get_hist_avg(avg_num, E_hist)
+    fid_hist_avg = get_hist_avg(avg_num, fid_hist)
+    param_idx_l_avg = get_hist_avg(avg_num, param_idx_l)
+
+    plt.rcParams['font.family'] = 'serif'
+    plt.rcParams['font.serif'] = ['Times New Roman'] + plt.rcParams['font.serif']
+    fig, ax = plt.subplots()
+    ax.scatter(param_idx_l_avg, E_hist_avg, alpha = 1, c = "#0072B2", s = 20, marker = ".", label = "Energy")
+    ax.set_xlabel('VQE Iterations', fontsize = 12)
+    ax.set_ylabel("Energy", fontsize = 12)
+    ax.legend(bbox_to_anchor=(1.28, 1.30), fontsize = 12)
+    ax2 = ax.twinx()
+    ax2.scatter(param_idx_l_avg, HR_dist_hist_avg, alpha = 1, c = "#D55E00", s = 20, marker=".", label = "HR distance")
+    ax2.scatter(param_idx_l_avg, fid_hist_avg, c = '#009E73', alpha = 0.8, marker=".", label = "Fidelity")
+    ax2.set_ylabel("HR distance | Fidelity", fontsize = 12)
+    ax2.legend(bbox_to_anchor=(1.28, 1.22), fontsize = 12)
+    plt.savefig(f"plot_2c_avg_{avg_num}.svg", dpi = 300, bbox_inches='tight')
+
 def make_plot():
     E_hist = load_l("noisy_E_10000_shots_aer_simulator__p1_0.0065_p2_0.0398.pkl")
     HR_dist_hist = load_l("HR_10000shots_aer_simulator_p1_0.0065_p2_0.0398.pkl")
@@ -34,48 +75,6 @@ def make_plot():
     ax2.set_ylabel("HR distance | Fidelity", fontsize = 12)
     ax2.legend(bbox_to_anchor=(1.28, 1.22), fontsize = 12)
     plt.savefig("plot_2d.svg", dpi = 300, bbox_inches='tight')
-
-def make_avg_plot():
-    """
-    function that creates a plot that averages HR distances to show better correlation with energy
-    truncates the plot at an appropriate energy if needed
-    """
-    #SET avg_num HERE:
-    avg_num = 2
-    HR_dist_hist = load_l("HR_10000shots_aer_simulator_p1_0.0065_p2_0.0398.pkl")
-    E_hist = load_l("noisy_E_10000_shots_aer_simulator__p1_0.0065_p2_0.0398.pkl")
-    fid_hist = load_l("fid_p1_0.0065_p2_0.0398.pkl")
-    param_idx_l = np.array(list(range(len(E_hist))))
-    if avg_num % 2 == 1:
-        n = avg_num // 2
-        param_idx_l_avg = param_idx_l[n: len(param_idx_l)-n]
-    else:
-        n = avg_num // 2 - 1
-        param_idx_l_trunc = param_idx_l[n: len(param_idx_l)-n]
-        param_idx_l_avg = []
-        for i in list(range(len(param_idx_l_trunc)-1)):
-            param_idx_avg = (param_idx_l_trunc[i] + param_idx_l_trunc[i+1])/2
-            param_idx_l_avg.append(param_idx_avg)
-    HR_dist_hist_avg = []
-    for idx in range(0, len(HR_dist_hist) - avg_num + 1):
-        HR_dist_avg = 0
-        for i in range(idx, idx+avg_num):
-            HR_dist_avg += HR_dist_hist[i]
-        HR_dist_avg = HR_dist_avg/avg_num
-        HR_dist_hist_avg.append(HR_dist_avg)
-    plt.rcParams['font.family'] = 'serif'
-    plt.rcParams['font.serif'] = ['Times New Roman'] + plt.rcParams['font.serif']
-    fig, ax = plt.subplots()
-    ax.scatter(param_idx_l, E_hist, alpha = 1, c = "#0072B2", s = 20, marker = ".", label = "Energy")
-    ax.set_xlabel('VQE Iterations', fontsize = 12)
-    ax.set_ylabel("Energy", fontsize = 12)
-    ax.legend(bbox_to_anchor=(1.28, 1.30), fontsize = 12)
-    ax2 = ax.twinx()
-    ax2.scatter(param_idx_l_avg, HR_dist_hist_avg, alpha = 1, c = "#D55E00", s = 20, marker=".", label = "HR distance")
-    ax2.scatter(param_idx_l, fid_hist, c = '#009E73', alpha = 0.8, marker=".", label = "Fidelity")
-    ax2.set_ylabel("HR distance | Fidelity", fontsize = 12)
-    ax2.legend(bbox_to_anchor=(1.28, 1.22), fontsize = 12)
-    plt.savefig(f"plot_2c_avg_{avg_num}.svg", dpi = 300, bbox_inches='tight')
 
 def main():
     make_avg_plot()
